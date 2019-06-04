cf30c133c4bdb2a2644d1d3519369aa2410d06ca
zhanlianghu <zhanliang.hu@gmail.com>
2019-06-04 16:12:17 +0800

    leaning dart language！

--- lib/exercise/TestThread.dart
+++ lib/exercise/TestThread.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
 
 void main() => runApp(new MaterialApp(
       title: "test thread",
@@ -12,8 +14,9 @@ class ThreadWidget extends StatefulWidget {
 }
 
 class ThreadState extends State<ThreadWidget> {
-  String _remoteData = "local data!";
+  String _remoteData = "click the bottom button get data!";
   int _clickTimes = 0;
+  HttpClient httpClient = new HttpClient();
 
   _showRemoteData() {//展示数据
     return new Center(
@@ -22,9 +25,21 @@ class ThreadState extends State<ThreadWidget> {
   }
 
   _onLoadData() async {//线程调用
+
+    // ignore: close_sinks
+    var request = await httpClient.getUrl(Uri.parse("http://www.weather.com.cn/data/cityinfo/101010100.html"));
+    var response = await request.close();
+    String res = "";
+    if (response.statusCode == HttpStatus.ok){
+//      res = response.connectionInfo.remoteAddress.host;
+    var data = await response.transform(utf8.decoder).join();
+    var jsonData = jsonDecode(data);
+    res = jsonData;
+    }
     setState(() {
-      _remoteData = "remote data " + '$_clickTimes';//字符串拼接
-      _clickTimes++;
+      _remoteData = res;//字符串拼接
+//      _remoteData = "remote data " + '$_clickTimes';//字符串拼接
+//      _clickTimes++;
     });
   }
 

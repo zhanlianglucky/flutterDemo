dd29a198a0beac7fc9d222089fcaff60d079a4d5
zhanlianghu <zhanliang.hu@gmail.com>
2019-06-26 14:52:25 +0800

    test net！

--- lib/iexercise/TestNet.dart
+++ lib/iexercise/TestNet.dart
@@ -3,7 +3,7 @@ import 'dart:convert';//字符集格式转换
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;//网络依赖
 
-///网络 新增http依赖库 参考地址：https://pub.dev/packages/http#-readme-tab-
+///网络+json数据解析   新增http依赖库 参考地址：https://pub.dev/packages/http#-readme-tab-
 void main() => runApp(new MaterialApp(
   title: "test net",
   home: new Scaffold(
@@ -19,19 +19,43 @@ class ContentWidget extends StatefulWidget {
   State<StatefulWidget> createState() => new ContentState();
 }
 
-class Weather {
+
+class WeatherInfo {
+  String city,cityid,temp1,temp2,weather,ptime;
+  WeatherInfo({this.city,this.cityid,this.temp1,this.temp2,this.weather,this.ptime});
+  factory WeatherInfo.formJson(Map<String,dynamic> map){
+    return new WeatherInfo(
+      city: map['city'],
+        cityid: map['cityid'],
+      temp1: map['temp1'],
+      temp2: map['temp2'],
+      weather: map['weather'],
+      ptime: map['ptime'],
+    );
+  }
 
 }
 
 class ContentState extends State<ContentWidget> {
 
   String data = "content";
+  WeatherInfo weatherInfo;
   _getDataFromNet() async {
     String url = "http://www.weather.com.cn/data/cityinfo/101010100.html";
     var res = await http.get(url);
     setState(() {
       data = utf8.decode(res.body.codeUnits);
+      var js = json.decode(data);
+      print("${js['weatherinfo']}");//去掉最外层
+      weatherInfo = new WeatherInfo.formJson(js["weatherinfo"]);//解析数据
     });
+  }
+  _getTestInfo() {
+    String str = data;
+    if (weatherInfo != null){
+      str = "${weatherInfo.city} ${weatherInfo.cityid} \n ${weatherInfo.weather} \n ${weatherInfo.temp1} \n ${weatherInfo.temp2} \n ${weatherInfo.ptime} \n";
+    }
+    return str;
   }
 
   @override
@@ -46,7 +70,7 @@ class ContentState extends State<ContentWidget> {
               onPressed: (){
             _getDataFromNet();
           }),
-          new Text(data),
+          new Text(_getTestInfo()),
         ],
       ),
     );
